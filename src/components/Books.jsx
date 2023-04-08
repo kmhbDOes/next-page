90c5bb1caff5d773bde3e57b31a7af7c2c71c946
kmhbDOes <kmhbipul@gmail.com>
2023-04-08 13:46:57 +0600

    Card Hover Added

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -8,8 +8,20 @@ const Books = () => {
     <div className="my-container">
       <div className="grid gap-6 mb-8 lg:grid-cols-4 sm:grid-cols-2">
         {books.map((book) => (
-          <div className="">
-            <img src={book.image} alt="book cover" />
+          <div className="overflow-hidden relative">
+            <img
+              src={book.image}
+              alt="book cover"
+              className="object-cover w-full h-56 md:h-64 xl:h-80"
+            />
+
+            <div className="bg-black text-gray-300 bg-opacity-75 opacity-0 hover:opacity-100 absolute inset-0">
+              <p>{book.title}</p>
+              <br />
+              <p>{book.subtitle}</p>
+              <br />
+              <p>Price: {book.price}</p>
+            </div>
           </div>
         ))}
       </div>
